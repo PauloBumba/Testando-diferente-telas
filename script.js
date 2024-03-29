e457177faf82bb338d3ffd35e1810aa351ec9ec5
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-03-29 18:03:43 -0300

    Testando diferenyes telas

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ function MenuBurger(){
         itens.style.display="none"
     }
     else{
-        itens.style.display="block"
+        itens.style.display="block";
+    }
+
+}
+function Mudartela(){
+    if (window.innerWidth>=768){
+        itens.style.display="block";
+    }
+    else{
+        itens.style.display="none";
     }
 }
